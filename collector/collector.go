6a5dcb09aad3032d798fc94cfc16cec4db57a510
Tim Snels <snelstim@me.com>
2024-05-17 22:55:36 +1200

    add printer status "state"

--- collector/collector.go
+++ collector/collector.go
@@ -475,6 +475,27 @@ func (c Collector) Collect(ch chan<- prometheus.Metric) {
 			prometheus.CounterValue,
 			result.Result.Status.PrintStats.FilamentUsed)
 
+		// Create a new descriptor for the print status metric
+		statusLabels := []string{"state"}
+		statusDesc := prometheus.NewDesc("klipper_print_print_status", "The status of the current print", statusLabels, nil)
+
+		// Map the status string to a numerical value
+		statusValue := map[string]float64{
+			"standby":   0,
+			"printing":  1,
+			"paused":    2,
+			"complete":  3,
+			"cancelled": 4,
+			"error":     5,
+		}
+
+		// Add the print status metric to the channel
+		ch <- prometheus.MustNewConstMetric(
+			statusDesc,
+			prometheus.GaugeValue,
+			statusValue[result.Result.Status.PrintStats.State], // Map the status string to its numerical value
+			result.Result.Status.PrintStats.State)              // Use the status string as a label value
+
 		// display_status
 		ch <- prometheus.MustNewConstMetric(
 			prometheus.NewDesc("klipper_print_gcode_progress", "The percentage of print progress, as reported by M73.", nil, nil),
