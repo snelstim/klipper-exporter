90c2c3ec52bcb1b9899e373d1704d47bd839df58
Tim Snels <15845326+snelstim@users.noreply.github.com>
2024-05-20 22:37:36 +1200

    Update spoolman.go

--- collector/spoolman.go
+++ collector/spoolman.go
@@ -14,8 +14,8 @@ type SpoolmanHealthQueryResponse struct {
 	} `json:"result"`
 }
 
-func (c Collector) fetchSpoolmanHealth(klipperHost string, apiKey string) (*SpoolmanHealthQueryResponse, error) {
-	var url = "http://" + klipperHost + ":7912/api/v1/health"
+func (c Collector) fetchSpoolmanHealth(spoolmanHost string, apiKey string) (*SpoolmanHealthQueryResponse, error) {
+	var url = "http://" + spoolmanHost + "/health"
 	log.Debug("Collecting metrics from " + url)
 
 	client := &http.Client{}
@@ -40,6 +40,52 @@ func (c Collector) fetchSpoolmanHealth(klipperHost string, apiKey string) (*Spoo
 	}
 
 	var response SpoolmanHealthQueryResponse
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+type SpoolmanFindSpoolQueryResponse struct {
+	Result struct {
+		Id       bool `json:"id"`
+		Filament []struct {
+			Id          int `json:"id"`
+			SpoolWeight int `json:"spool_weight"`
+		} `json:"filament"`
+	} `json:"result"`
+}
+
+func (c Collector) fetchSpoolmanFindSpool(spoolmanHost string, apiKey string) (*SpoolmanFindSpoolQueryResponse, error) {
+	var url = "http://" + spoolmanHost + "/spool"
+	log.Debug("Collecting metrics from " + url)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	if apiKey != "" {
+		req.Header.Set("X-API-KEY", apiKey)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	var response SpoolmanFindSpoolQueryResponse
 
 	err = json.Unmarshal(data, &response)
 	if err != nil {
